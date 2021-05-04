fa6ff36066f7b9900bcba34b87329f4de71a36b4
Darian Sereno <dasereno@student.42.fr>
2021-05-04 23:19:24 +0200

    target indicator implemented

--- src/ui/game/int_to_str.c
+++ src/ui/game/int_to_str.c
@@ -36,3 +36,18 @@ char *int_to_str(int nb)
     my_revstr(result);
     return (result);
 }
+
+char *number_and_string(int index, char *str)
+{
+    char *nb = int_to_str(index);
+    char *res = my_malloc(sizeof(char) * (my_strlen(str) + my_strlen(nb) + 1));
+
+    res[0] = '\0';
+    if (index == 0) {
+        res = my_strcat(res, str);
+        res = my_strcat(res, '0\0');
+    }
+    res = my_strcat(res, str);
+    res = my_strcat(res, nb);
+    return (res);
+}
