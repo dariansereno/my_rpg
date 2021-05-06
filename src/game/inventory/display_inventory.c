e43330ed30f50ecf3a0ce19fa29680870e25b7d7
Darian Sereno <dasereno@student.42.fr>
2021-05-06 17:55:35 +0200

    color text

--- src/game/inventory/display_inventory.c
+++ src/game/inventory/display_inventory.c
@@ -11,7 +11,7 @@ void display_item(st_global *ad, st_ressources item, sfVector2f pos)
 {
     sfSprite_setPosition(ad->items[item.id]->sprite, pos);
     sfText_setString(item.text, itoa(item.nb, ad->nb_inv, 10));
-    sfText_setFillColor(item.text, sfWhite);
+    sfText_setColor(item.text, sfWhite);
     sfText_setCharacterSize(item.text, 25);
     sfText_setPosition(item.text, (sfVector2f){pos.x + 25 + ((item.id - 4) * -1) * 3, pos.y + 20});
     sfRenderWindow_drawSprite(ad->window->window, ad->items[item.id]->sprite,
