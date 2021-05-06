e498a5507a87a737678b2195bbb3920066d8d8e8
Delagnes Evan <evan.delagnes@epitech.eu>
2021-05-06 23:38:29 +0200

    boss + msg fix

--- src/messages/set_message.c
+++ src/messages/set_message.c
@@ -7,15 +7,16 @@
 
 #include "my_rpg.h"
 
-void dialogue_text_settings(st_global *global)
+void dialogue_text_settings(st_global *g)
 {
-    global->text->str = automatically_set_text_max(global);
-    sfText_setCharacterSize(global->text->text, 18);
-    sfText_setFont(global->text->text, global->text->font);
-    sfText_setLineSpacing(global->text->text, 2.5);
-    sfText_setColor(global->text->text, sfBlack);
-    sfText_setString(global->text->text, global->text->str);
-    sfText_setPosition(global->text->text, (sfVector2f){385, 960});
+    g->text->str = automatically_set_text_max(g);
+    sfText_setCharacterSize(g->text->text, 18);
+    sfText_setFont(g->text->text, g->text->font);
+    sfText_setLineSpacing(g->text->text, 2.5);
+    sfText_setColor(g->text->text, sfBlack);
+    sfText_setString(g->text->text, g->text->str);
+    sfText_setPosition(g->text->text, (sfVector2f){385 + g->ship->viewrect.left,
+    960 + g->ship->viewrect.top});
 }
 
 void dialogue_rectangle_settings(st_global *g)
@@ -23,9 +24,11 @@ void dialogue_rectangle_settings(st_global *g)
     sfRectangleShape_setSize(g->text->whi_r, (sfVector2f){1180, 130});
     sfRectangleShape_setSize(g->text->whi_r2, (sfVector2f){1180, 130});
     sfRectangleShape_setPosition(g->text->whi_r, \
-    (sfVector2f){(WIDTH - 1180) / 2, HEIGHT - 140});
+    (sfVector2f){((WIDTH - 1180) / 2) +  g->ship->viewrect.left, (HEIGHT - 140)
+    + g->ship->viewrect.top});
     sfRectangleShape_setPosition(g->text->whi_r2, \
-    (sfVector2f){(WIDTH - 1180) / 2, HEIGHT - 140});
+    (sfVector2f){((WIDTH - 1180) / 2) + g->ship->viewrect.left, \
+    (HEIGHT - 140) + g->ship->viewrect.top});
     sfRectangleShape_setFillColor(g->text->whi_r, \
     sfColor_fromRGBA(255, 255, 255, 0));
     sfRectangleShape_setOutlineColor(g->text->whi_r, \
@@ -48,10 +51,14 @@ void dialogue_rectangle_settings_bis(st_global *g)
     sfRectangleShape_setRotation(g->text->text_r2, 180);
     sfRectangleShape_setRotation(g->text->text_r3, 180);
     sfRectangleShape_setRotation(g->text->text_r4, 180);
-    sfRectangleShape_setPosition(g->text->text_r1, (sfVector2f){1545, 960});
-    sfRectangleShape_setPosition(g->text->text_r2, (sfVector2f){1545, 990});
-    sfRectangleShape_setPosition(g->text->text_r3, (sfVector2f){1545, 1020});
-    sfRectangleShape_setPosition(g->text->text_r4, (sfVector2f){1545, 1050});
+    sfRectangleShape_setPosition(g->text->text_r1, (sfVector2f)\
+    {1545 + g->ship->viewrect.left, 960 + g->ship->viewrect.top});
+    sfRectangleShape_setPosition(g->text->text_r2, (sfVector2f)\
+    {1545 + g->ship->viewrect.left, 990 + g->ship->viewrect.top});
+    sfRectangleShape_setPosition(g->text->text_r3, (sfVector2f)\
+    {1545 + g->ship->viewrect.left, 1020 + g->ship->viewrect.top});
+    sfRectangleShape_setPosition(g->text->text_r4, (sfVector2f)\
+    {1545 + g->ship->viewrect.left, 1050 + g->ship->viewrect.top});
 }
 
 void dialogue_text(st_global *global)
@@ -68,7 +75,8 @@ void add_message(st_global *global, char *str, int delay)
     global->text->arrow_t = \
     sfTexture_createFromFile("contents/ui/textures/arrow.png", NULL);
     sfSprite_setTexture(global->text->arrow_s, global->text->arrow_t, sfTrue);
-    sfSprite_setPosition(global->text->arrow_s, (sfVector2f){1528, 1045});
+    sfSprite_setPosition(global->text->arrow_s, (sfVector2f)\
+    {1528 + g->ship->viewrect.left, 1045 + g->ship->viewrect.top});
     global->text->whi_r = sfRectangleShape_create();
     global->text->whi_r2 = sfRectangleShape_create();
     global->text->text_r1 = sfRectangleShape_create();
