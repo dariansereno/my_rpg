0932c83373bd29fa1d95026b7e57215141f79719
Darian <darian.sereno@epitech.eu>
2021-05-08 13:16:08 +0200

    boss anims

--- src/game/boss/boss_fight/boss_atk.c
+++ src/game/boss/boss_fight/boss_atk.c
@@ -7,6 +7,31 @@
 
 #include "my_rpg.h"
 
+int random_piped_four(void)
+{
+    int rand = random_between(1, 20);
+    int x = 0;
+
+    switch (x) {
+    case 0:
+        if (rand <= 6)
+            return (1);
+        x++;
+    case 1:
+        if (rand > 6 && rand <= 12)
+            return (2);
+        x++;
+    case 2:
+        if (rand > 12 && rand <= 18)
+            return (3);
+        x++;
+    case 3:
+        if (rand > 18 && rand <= 20)
+            return (4);
+        x++;
+    }
+}
+
 void boss_atk(st_global *ad)
 {
     if (!ad->var_game->boss_generated)
@@ -17,7 +42,8 @@ void boss_atk(st_global *ad)
     time = sfClock_getElapsedTime(ad->boss->atk_timer->clock);
     second = time.microseconds / 1000000.0;
     if (second > 1) {
-        push_back_boss(&ad->boss->atk_li, random_between(5, 10), random_between(2, 2));
+        push_back_boss(&ad->boss->atk_li, random_between(3, 6),
+        random_piped_four());
         sfClock_restart(ad->boss->atk_timer->clock);
     }
 }
--- src/map/planets/generation/generate_map_planets.c
+++ src/map/planets/generation/generate_map_planets.c
@@ -66,7 +66,7 @@ int *generate_mod_price()
 st_planet_global *generate_all_map(void)
 {
     scatter math = {.germ = 469880, .interval = 80000, .kmax =
-    random_between(2000, 2500), .p = 1288, .q = 1664713};
+    /*random_between(2000, 2500)*/ 8, .p = 1288, .q = 1664713};
     sfVector2i *pos = scatter_plot(math);
     st_planet_global *planets = my_malloc(sizeof(*planets));
     planets->planets = NULL;
